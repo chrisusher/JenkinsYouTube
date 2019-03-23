09f95ac4c350cea640aa0f16e615cf3d18628002
Chris Usher <chris.usher23@gmail.com>
2019-03-23 21:45:56 +0000

    First commit of OctoFX

---
+++ OctoFX/OctoFX.SmokeTests/SmokeTestFixture.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using OctoFX.SmokeTests.Properties;
+using System.Net;
+
+namespace OctoFX.SmokeTests
+{
+   [TestFixture]
+   public class SmokeTestFixture
+   {
+      #region Public Methods
+
+      [Test]
+      public void EnsureWebSiteIsRunning()
+      {
+         string responseText = new WebClient().DownloadString(Settings.Default.endpointUrl);
+
+         Assert.That(responseText, Does.Contain("OctoFX makes it easy to buy and sell"));
+      }
+
+      #endregion Public Methods
+   }
+}
